5a6e6548117bc022d9ea658e409fbca3d6ba5d6f
Kyuyeon <kyuyeonpooh@gmail.com>
2020-05-21 20:51:01 +0900

    Revise some preprocess configurations

--- preprocess.py
+++ preprocess.py
@@ -1,25 +1,25 @@
-import os
 import configparser
-from util import extractor
+import os
+
+from easydict import EasyDict
+
+from util.extractor import Extractor
 
 # open configuration file
 config_file = "config.ini"
 config = configparser.ConfigParser()
 config.read(config_file)
 
-# extract configurations
-data_dir = config["DATA_DIR"]
-src_vid_dir = data_dir["src_video_dir"]
-src_aud_dir = data_dir["src_audio_dir"]
-out_vid_dir = data_dir["out_video_dir"]
-out_aud_dir = data_dir["out_audio_dir"]
+# extract configurations with EasyDict
+config_dict = {section: dict(config.items(section)) for section in config.sections()}
+config = EasyDict(config_dict)
 
-# raw data file naming conventions
-data_file = config["DATA_FILE"]
-vid_file_head = data_file["vid_file_head"]
-aud_file_head = data_file["aud_file_head"]
-
+"""
+please run this file in root directory not in "utils" folder
+$ python preprocess.py          (X)
+$ python utils/preprocess.py    (O)
+"""
 if __name__ == "__main__":
     # preprocess video and audio into npz file
-    ext = extractor.Extractor(src_vid_dir, src_aud_dir, out_vid_dir, out_aud_dir, vid_file_head, aud_file_head)
-    ext.run(ncpu=8)
+    extractor = Extractor(src_vid_dir, src_aud_dir, out_vid_dir, out_aud_dir, vid_file_head, aud_file_head)
+    extractor.run(ncpu=8)
