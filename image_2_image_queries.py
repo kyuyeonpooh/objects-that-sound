b42fe16bb31779a3fbe905b0ad5a7a761778418c
hyeongyeolryu <hyeongyeolryu@gmail.com>
2020-06-03 22:27:58 +0900

    Add function of saving query results

--- image_2_image_queries.py
+++ image_2_image_queries.py
@@ -1,10 +1,14 @@
 from matplotlib import pyplot as plt
-from utils.util import getNumToTagsMap, bgr2rgb
+from utils.util import getNumToTagsMap, bgr2rgb, save_result
 import torch
 import numpy as np
 
 
-def imageToImageQueries(embeddings='savedEmbeddings.pt', topk=5, use_tags=False):
+def imageToImageQueries(embeddings=None, 
+                        topk=5, 
+                        use_tags=False, 
+                        result_path=None
+                        ):
 
     finalTag = getNumToTagsMap()
     #print(finalTag)
@@ -26,12 +30,11 @@ def imageToImageQueries(embeddings='savedEmbeddings.pt', topk=5, use_tags=False)
             )
 
     print("Loaded embeddings.")
-    #imgList = bgr2rgb(imgList)
-    flag = True
 
-        # Open a file and store your queries here
-    res = open("results/results_{0}_{1}.txt".format("im", "im"), "w+")
+    #flag = True
 
+    res_queries = []
+    res_tags = []
     for i in range(imgEmbedList.shape[0]):
         embed = imgEmbedList[i]
         dist  = ((embed - imgEmbedList)**2).sum(1)
@@ -48,29 +51,35 @@ def imageToImageQueries(embeddings='savedEmbeddings.pt', topk=5, use_tags=False)
         plt.axis("off")
         plt.imshow(imgList[idx[0]].transpose(1,2,0))
         
-        res_tags = []
+        res_tag = []
         for j in range(1, num_fig):
             ax = plt.subplot(2, 3, j+1 + int(j/3))
             if use_tags:
                 ax.set_title(finalTag[vidTagList[idx[j]]])
-                res_tags.append(finalTag[vidTagList[idx[j]]])
+                res_tag.append(finalTag[vidTagList[idx[j]]])
             plt.imshow(imgList[idx[j]].transpose(1,2,0))
             plt.axis("off")
 
         # plt.tight_layout()
-        plt.draw()
-        plt.pause(0.001)
-        if flag:
-            input()
-            flag = False
+        #plt.draw()
+        #plt.pause(0.001)
+        #if flag:
+        #    input()
+        #    flag = False
         # res = raw_input("Do you want to save?")
         # if res == "y":
         plt.savefig("results/embed_im_im_{0}.png".format(i))
         
-        res_input = "query: " + res_query + ", result: " + (",".join(res_tags))
-        res.write(res_input + "\n")
-    res.close()
+        res_queries.append(res_query)
+        res_tags.append(res_tag)    
+    save_result(result_path, res_queries, res_tags)
+
 
 if __name__ == "__main__":
     embedding_path = './save/embeddings/savedEmbeddings.pt'
-    imageToImageQueries(embeddings=embedding_path, topk=5, use_tags=True)
+    result_path = './results/results.pickle'
+    imageToImageQueries(embeddings=embedding_path, 
+                        topk=5,
+                        use_tags=True, 
+                        result_path=result_path
+                        )
